a41a70c173bc6baa0c45e9147a9bc6e8ff5288c1
Dalibor Dragojevic <dalibor.dragojevic@computerrock.com>
2019-09-16 13:30:21 +0200

    api server WIP

--- api/db_init.js
+++ api/db_init.js
@@ -4,12 +4,14 @@ let Schema = mongoose.Schema;
 
 let ItemSchema = new Schema(
     {
+        id: Number,
         img:
             { data: Buffer, contentType: String, id: Number },
         name: String,
         author: String,
         synopsis: String,
-        year: Number
+        year: Number,
+        category: String
     }
 );
 let Item = mongoose.model('Books',ItemSchema);
@@ -21,6 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017').then(() => {
             ++i;
             console.log(file);
             let newItem = new Item();
+            newItem.id = i;
             newItem.img.data = fs.readFileSync('./data/img/' + file);
             newItem.img.contentType = 'image/jpg';
             newItem.img.id = i;
@@ -28,6 +31,19 @@ mongoose.connect('mongodb://127.0.0.1:27017').then(() => {
             newItem.author = 'Paul Sheldon';
             newItem.synopsis = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
             newItem.year = 2018;
+            switch (true) {
+                case (i < 15):
+                    newItem.category = 'Fiction';
+                    break;
+                case (i < 22):
+                    newItem.category = 'Philosophy';
+                    break;
+                case (i < 28):
+                    newItem.category = 'Poetry';
+                    break;
+                default:
+                    newItem.category = 'Philosophy';
+            }
             newItem.save();
         }
     });
