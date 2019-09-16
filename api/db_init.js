ece58110f29041c7fffb59721a2009dfcb867ee2
Dalibor Dragojevic <dalibor.dragojevic@computerrock.com>
2019-09-16 12:53:06 +0200

    correct db init code

--- api/db_init.js
+++ api/db_init.js
@@ -5,23 +5,28 @@ let Schema = mongoose.Schema;
 let ItemSchema = new Schema(
     {
         img:
-            { data: Buffer, contentType: String },
+            { data: Buffer, contentType: String, id: Number },
         name: String,
         author: String,
+        synopsis: String,
         year: Number
     }
 );
 let Item = mongoose.model('Books',ItemSchema);
 
 mongoose.connect('mongodb://127.0.0.1:27017').then(() => {
+    let i = 0;
     fs.readdirSync('./data/img').forEach(file => {
         if (/.jpg/.test(file)) {
+            ++i;
             console.log(file);
             let newItem = new Item();
             newItem.img.data = fs.readFileSync('./data/img/' + file);
             newItem.img.contentType = 'image/jpg';
+            newItem.img.id = i;
             newItem.name = 'A book';
             newItem.author = 'Paul Sheldon';
+            newItem.synopsis = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
             newItem.year = 2018;
             newItem.save();
         }
