7e4903795080ed64c19abd2cacab4fa77f36debb
Dalibor Dragojevic <dalibor.dragojevic@computerrock.com>
2019-12-09 12:02:50 +0100

    add foundation for select widget

---
+++ client-src/src/elements/custom-select/CustomSelect.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./CustomSelect.scss";
+
+class CustomSelect extends Component {
+  static propTypes = {
+    label: PropTypes.string,
+    children: PropTypes.node,
+    className: PropTypes.string
+  };
+
+  static defaultProps = {
+    label: null,
+    className: null,
+    children: null
+  };
+
+  state = {
+    isActive: false
+  };
+
+  toggleDropDown = e => {
+    this.setState(state => ({ isActive: !state.isActive }));
+  };
+
+  render() {
+    const { label, children, className } = this.props;
+
+    return (
+      <div
+        onClick={this.toggleDropDown}
+        className={`vub-c-custom-select ${className || ""}`}
+        tabIndex="0"
+      >
+        {label && <label className="vub-c-custom-select__label">{label}</label>}
+        <div className="vub-c-custom-select__field">
+          <span className="vub-c-custom-select__field-value">Select field</span>
+          <div className="vub-c-custom-select__icon" />
+        </div>
+        {this.state.isActive && (
+          <ul className="vub-c-custom-select__drop-down" tabIndex="0">
+            {children}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
+
+export default CustomSelect;
--- client-src/src/view-profile/ProfileView.js
+++ client-src/src/view-profile/ProfileView.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import CustomSelect from "../elements/custom-select/CustomSelect";
+import "./ProfileView.scss";
 
 class ProfileView extends Component {
   constructor(props) {
@@ -13,7 +15,13 @@ class ProfileView extends Component {
   componentWillUnmount() {}
 
   render() {
-    return <p>Insert custom select widget</p>;
+    return (
+      <CustomSelect label="Sort by" className="bs-c-select-widget-container">
+        <p>Click A</p>
+        <p>Click B</p>
+        <p>Click C</p>
+      </CustomSelect>
+    );
   }
 }
 
